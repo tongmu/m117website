7fc6fc3ee0a634d2f349d7236b963718c77247d4
tongmu <tongmu28@gmail.com>
2015-11-14 15:22:20 -0800

    change

--- healthplus.js
+++ healthplus.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
                         setInterval(function () {
                             var x = (new Date()).getTime(), // current time
                                 y = Math.random() * 150;
-                            z = Math.round(Math.random() * 3);
+                            z = Math.ceil(Math.random() * 2);
                             heartRate.addPoint([x, y], true, true);
                             activity.addPoint([x, z], false, true);
                         }, 1000);
@@ -98,7 +98,7 @@ $(document).ready(function () {
                         for (i = -19; i <= 0; i++) {
                             data.push({
                                 x: time + i * 1000,
-                                y: Math.round(Math.random() * 3)
+                                y: Math.ceil(Math.random() * 2)
                             });
                         }
                         return data;
